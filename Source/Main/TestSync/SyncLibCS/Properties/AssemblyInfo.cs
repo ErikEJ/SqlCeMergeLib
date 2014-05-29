e91087bd33dec9813d290b3af3392a2c71ac2c6f
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2014-05-29 10:54:03 +0000

    adding control of parameters as per http://robtiffany.com/mobile-merge-replication-performance-and-scalability-cheat-sheet/

--- Source/Main/TestSync/SyncLibCS/Properties/AssemblyInfo.cs
+++ Source/Main/TestSync/SyncLibCS/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ErikEJ.SqlCeMergeLib")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
