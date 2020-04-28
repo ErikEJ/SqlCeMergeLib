6a0867eec7d86ba9f9072d006e0616580831fff9
ErikEJ <ejlskov@hotmail.com>
2020-04-28 18:55:50 +0200

    Packing updates

--- Source/Main/TestSync/SyncLibCS/Properties/AssemblyInfo.cs
+++ Source/Main/TestSync/SyncLibCS/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ErikEJ.SqlCeMergeLib")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
